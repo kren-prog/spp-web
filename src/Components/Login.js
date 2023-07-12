e51adb7e5987a31eb2c6b7a6de60c73d9426faad
kren-prog <karenpg40@gmail.com>
2023-07-11 22:49:33 -0500

    center login

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Form, Container, Button, Card, Image, InputGroup, FormControl } from "react-bootstrap";
+import { Form, Container, Button, Card, Image, InputGroup } from "react-bootstrap";
 import { KeyFill, PersonFill, Eye, EyeSlash } from "react-bootstrap-icons";
 import svgI from "../assets/images/astronaut.svg";
 import { useNavigate } from 'react-router-dom';
@@ -35,63 +35,77 @@ function Login() {
     }
 
     return (
-        <Container className="d-flex my-3">
-            <Card className="text-center mx-auto bg-light">
-                <Image
-                    className="mx-auto mt-2"
-                    height={171}
-                    width={150}
-                    src={svgI}
-                />
-                <Card.Body className="pt-0">
-                    <Card.Header className="bg-warning text-white">
-                        <span>Bienvenido</span>
-                    </Card.Header>
-                    <Form onSubmit={login}>
-                        <InputGroup className="my-3">
-                            <InputGroup.Text id="basic-addon1"><PersonFill /></InputGroup.Text>
-                            <Form.Control
-                                className="shadow-none"
-                                placeholder="Usuario"
-                                aria-label="Username"
-                                aria-describedby="basic-addon1"
-                                value={name}
-                                onChange={(e) => setName(e.target.value)}
-                                autoFocus
-                            />
-                        </InputGroup>
+        <Container className="">
 
-                        <InputGroup className="justify-content-end">
-                            <InputGroup >
-                                <InputGroup.Text id="basic-addon2"><KeyFill /> </InputGroup.Text>
-                                <Form.Control
-                                    type={showPassword ? "text" : "password"}
-                                    className="shadow-none pr-4"
-                                    placeholder="Contraseña"
-                                    onChange={handleChange}
-                                />
-                                <Button
-                                    variant="white"
-                                    className="my-1 mr-1 px-0 btn-sm bg-white shadow-none "
+            <div className='row' style={{
+                "display": "flex",
+                "align-items": "center",
+                "justify-content": "center",
+                "min-height": "75vh"
+            }}>
+                <div class="col-12 col-sm-10 col-lg-6 col-xl-4">
 
-                                    onMouseDown={onMouseDown}
-                                    onMouseUp={onMouseUp}
-                                    onMouseLeave={onMouseUp}
-                                    onTouchStart={onMouseDown}
-                                    onTouchEnd={onMouseUp}>
-                                    {showEye ? showPassword ? <EyeSlash /> : <Eye /> : null}
-                                </Button>
+                    <Card className="text-center mx-auto bg-light">
+                        <Image
+                            className="mx-auto mt-2"
+                            height={171}
+                            width={150}
+                            src={svgI}
+                        />
+                        <Card.Body className="pt-0">
+                            <Card.Header className="bg-warning text-white">
+                                <span>Bienvenido</span>
+                            </Card.Header>
+                            <Form onSubmit={login}>
+                                <InputGroup className="my-3">
+                                    <InputGroup.Text id="basic-addon1"><PersonFill /></InputGroup.Text>
+                                    <Form.Control
+                                        className="shadow-none"
+                                        placeholder="Usuario"
+                                        aria-label="Username"
+                                        aria-describedby="basic-addon1"
+                                        value={name}
+                                        onChange={(e) => setName(e.target.value)}
+                                        autoFocus
+                                    />
+                                </InputGroup>
 
-                            </InputGroup>
+                                <InputGroup className="justify-content-end">
+                                    <InputGroup >
+                                        <InputGroup.Text id="basic-addon2"><KeyFill /> </InputGroup.Text>
+                                        <Form.Control
+                                            type={showPassword ? "text" : "password"}
+                                            className="shadow-none pr-4"
+                                            placeholder="Contraseña"
+                                            onChange={handleChange}
+                                        />
+                                        <Button
+                                            variant="white"
+                                            className="my-1 mr-1 px-0 btn-sm bg-white shadow-none "
+
+                                            onMouseDown={onMouseDown}
+                                            onMouseUp={onMouseUp}
+                                            onMouseLeave={onMouseUp}
+                                            onTouchStart={onMouseDown}
+                                            onTouchEnd={onMouseUp}>
+                                            {showEye ? showPassword ? <EyeSlash /> : <Eye /> : null}
+                                        </Button>
+
+                                    </InputGroup>
 
 
-                        </InputGroup>
+                                </InputGroup>
 
 
-                        <Button type="submit" variant="primary" className='w-75 mt-2'>Iniciar sesion</Button>
-                    </Form>
-                </Card.Body>
-            </Card>
+                                <Button type="submit" variant="primary" className='w-75 mt-2'>Iniciar sesion</Button>
+                            </Form>
+                        </Card.Body>
+                    </Card>
+
+                </div>
+
+            </div>
+
         </Container>
     )
 }
@@ -99,50 +113,4 @@ function Login() {
 export default Login
 
 
-{/* 
-    <InputGroup className="my-3">
-                        <InputGroup.Prepend>
-                            <InputGroup.Text>
-                                <PersonFill />
-                            </InputGroup.Text>
-                        </InputGroup.Prepend>
-                        <FormControl className="shadow-none" placeholder="Username" />
-                    </InputGroup>
 
-<InputGroup className="my-3">
-<InputGroup.Prepend>
-    <InputGroup.Text>
-        <PersonFill />
-    </InputGroup.Text>
-</InputGroup.Prepend>
-<FormControl className="shadow-none" placeholder="Username" />
-</InputGroup>
-
-
-
-<InputGroup className="justify-content-end">
-<InputGroup.Prepend>
-    <InputGroup.Text>
-        <KeyFill />
-    </InputGroup.Text>
-</InputGroup.Prepend>
-<Button
-    variant="white"
-    className="my-1 mr-1 px-0 btn-sm bg-white shadow-none position-absolute"
-    style={{ zIndex: 4 }}
-    onMouseDown={onMouseDown}
-    onMouseUp={onMouseUp}
-    onMouseLeave={onMouseUp}
-    onTouchStart={onMouseDown}
-    onTouchEnd={onMouseUp}>
-    {showEye ? showPassword ? <EyeSlash /> : <Eye /> : null}
-</Button>
-<FormControl
-    type={showPassword ? "text" : "password"}
-    className="shadow-none pr-4"
-    placeholder="Password"
-    onChange={handleChange}
-/>
-</InputGroup> 
-
-*/}
